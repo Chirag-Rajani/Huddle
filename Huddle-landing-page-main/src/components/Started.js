2bf814a473d25df542330779a8663646b0456e62
Chirag-Rajani <86966905+Chirag-Rajani@users.noreply.github.com>
2023-05-18 11:26:50 +0100

    Initial commit

---
+++ Huddle-landing-page-main/src/components/Started.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Started = () => {
+  return (
+    <div className="started-container">
+      <div className="started">
+        <h3>Ready To Build Your Community?</h3>
+        <button className="started--icon">Get Started For Free</button>
+      </div>
+    </div>
+  );
+};
+
+export default Started;
